aac09e026ac38ade7d0171b7fc527bbeaee82aa9
Zack Siri <zack@artellectual.com>
2014-06-04 17:53:55 +0700

    added hash example for enum

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   include Analytix::Model
 
   enum status: [:draft, :published]
+  # enum status: { published: 1, draft: 0 }
 
   validates :title, presence: true
   has_many :comments
