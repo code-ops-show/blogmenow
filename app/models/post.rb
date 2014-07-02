74b5421321e8f19a00b6cc873fee8d7b5d800539
Zack Siri <zack@codemy.net>
2014-07-03 03:28:10 +0700

    applied custom query

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,29 @@ class Post < ActiveRecord::Base
   has_many :tags, through: :taggings
 
   belongs_to :category
+
+  scope :with_tags_and_count, -> { select([arel_table[Arel.star], Post.array_of_tags_hstore]) }
+ 
+  class << self
+    def array_of_tags_hstore
+      Arel::Nodes::NamedFunction.new("ARRAY", [tags_hstore]).as("tags_with_count")
+    end
+ 
+    def tags_hstore
+      Tag.arel_table.project(Arel.sql('hstore(tag)')).from(tags_from_taggings)
+    end
+ 
+    def tags_from_taggings
+      Tag.joins(:taggings)
+           .select(Tag.arel_table[:name], Tag.arel_table[:posts_count])
+             .from(tags_with_posts_count)
+               .where(Tagging.arel_table[:post_id].eq(arel_table[:id])).as('tag')
+    end
+ 
+    def tags_with_posts_count
+      Tag.joins(:posts)
+           .select(Tag.arel_table[Arel.star], Arel::Nodes::NamedFunction.new("COUNT", [arel_table[:id]])
+             .as('posts_count')).group(Tag.arel_table[:id]).as('tags')
+    end
+  end
 end
